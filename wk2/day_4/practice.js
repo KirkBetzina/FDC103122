398986ef5c914a0231ab628fd06249df1437bea6
kirk <kirk.betzina@gmail.com>
2022-11-10 22:26:21 -0500

    wk2 day4 practice

--- wk2/day_4/practice.js
+++ wk2/day_4/practice.js
@@ -3,7 +3,8 @@
 // Change following elements using ONLY JavaScript and the DOM methods.
 
 // 1. Change the inner text of the first <h1> to “I’m using JavaScript!!”
-
+document.getElementById('title').innerText = "I'm using JS";
 // 2. Change the background color of the first <div> with the id of animalContainer to blue
-
+document.getElementById('animalContainer').style.backgroundColor = "coral";
 // 3. Give all the <div> a black border
+document.getElementsByTagName('div').style.border = '#000000'
