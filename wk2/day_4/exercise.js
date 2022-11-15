ae8741413a3739b63a6e320eb75aa57885b3e09f
kirk <kirk.betzina@gmail.com>
2022-11-14 19:44:16 -0500

    wk2 day4 homework

--- wk2/day_4/exercise.js
+++ wk2/day_4/exercise.js
@@ -1,5 +1,18 @@
 // Use Flexbox to place the three divs into columns, spaced evenly, with the letter centered in the middle of each column.
 
 // Use JavaScript to change the background color of the A,B, & C divs to the color of their border.  Use Javascript to change the INNER HTML of the A,B, & C divs to the NAME of the color (ie, #A should read 'red', #B should read 'green', etc.)
+const A = document.getElementById('A');
+const B = document.getElementById('B');
+const C = document.getElementById('C');
+
+A.innerHTML = 'Red';
+B.innerHTML = 'Green';
+C.innerHTML = 'Yellow';
+
+A.style.backgroundColor = 'red';
+B.style.backgroundColor = 'green';
+C.style.backgroundColor =  'yellow';
+
 
 // Create a header and a footer.
+
