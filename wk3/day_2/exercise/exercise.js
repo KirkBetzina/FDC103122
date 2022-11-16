5fff6710b6db9d46e23d8109ef8cec8e0646f112
kirk <kirk.betzina@gmail.com>
2022-11-16 18:54:02 -0500

    initial set up of homework

--- wk3/day_2/exercise/exercise.js
+++ wk3/day_2/exercise/exercise.js
@@ -7,3 +7,48 @@
 // check out zola.com and see the carousel hero image!
 
 // NOTE: yours only needs to be triggered by buttons.
+// copy the completed HTML, CSS and JavaScript file into the files in this folder
+
+// add buttons below the div A div B div C
+
+// in each div, create a button with value change something
+const A = document.getElementById('A');
+const B = document.getElementById('B');
+const C = document.getElementById('C');
+
+// A.innerHTML = 'Red';
+// B.innerHTML = 'Green';
+// C.innerHTML = 'Yellow';
+
+A.style.backgroundColor = 'red';
+B.style.backgroundColor = 'green';
+C.style.backgroundColor =  'yellow';
+
+//////////////////
+// homework for day 2 section below //
+//////////////////
+
+const btnA = document.getElementById('btnA');
+const btnB = document.getElementById('btnB');
+const btnC = document.getElementById('btnC');
+
+function ChangeA () {
+    btnA.style = 'font-style: italic;'
+
+};
+
+
+function ChangeB () {
+    btnB.style = 'font-weight: 900'
+    
+};
+
+
+function ChangeC () {
+    alert("youve clicked on C");
+};
+
+
+///////////////////////////////////////////////
+// Homework for Day 3
+///////////////////////////////////////////////
