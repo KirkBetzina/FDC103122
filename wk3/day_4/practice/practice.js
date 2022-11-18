d63dbba4686bfb14d1ed9e2370a2c95ece3de3c3
kirk <kirk.betzina@gmail.com>
2022-11-17 21:47:03 -0500

    added day_4

---
+++ wk3/day_4/practice/practice.js
@@ -0,0 +1,12 @@
+/*
+TO DO APP
+
+1. EASY: First write out the function that will add on to the existing list from the users input.
+2. EASY: Create a button at the bottom of the list to clear out the whole list.
+3. HARD: With each item that is added, also add a delete button. NOTE: need a running count to keep track of which id element to delete.
+4. NIGHTMARE: Using the running count, can you figure out a way to edit the task? NOTE: need to add an edit button
+*/
+
+function addTask() {
+  alert("change me!");
+}
