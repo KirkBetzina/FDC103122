d85b34c6258384194515564d45a0e32c575717ff
kirk <kirk.betzina@gmail.com>
2022-11-21 19:58:21 -0500

    adding the practice code from class

--- wk3/day_4/practice/practice.js
+++ wk3/day_4/practice/practice.js
@@ -7,6 +7,32 @@ TO DO APP
 4. NIGHTMARE: Using the running count, can you figure out a way to edit the task? NOTE: need to add an edit button
 */
 
-function addTask() {
-  alert("change me!");
-}
+// ***PSEUDOCODE FOR NIGHTMARE MODE***
+
+//create counter
+
+//add task function
+  //assign task to variable
+  //create html div
+  //assign li id
+  //update li innerhtml with task variable
+  //append li to ul  
+  //update counter
+  //clear entry field
+
+//edit task function
+  //define element number to be editd
+  //access li element in the dom based on the toEdit value
+  //define changes from element
+  //update innerHTML with changes
+  //clear the form fields
+  
+//delete task function
+  //define element number to be deleted
+  //access li element in the dom based on the toDelete value
+  //update css to strikethrough text
+  //clear form fields
+    
+//clear all function
+  //update taskCol to its original state
+  //reset counter
